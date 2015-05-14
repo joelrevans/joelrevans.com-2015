521cb9e7ef45d7e297ce5e535230f9997485af58
Joel Evans <joel.evans@cliftonlabs.com>
2015-05-14 11:36:03 -0400

    Created a helper to embed script files directly onto the page.

--- joelrevans.com_v5/joelrevans.com_v5/Controllers/MainController.cs
+++ joelrevans.com_v5/joelrevans.com_v5/Controllers/MainController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO.Compression;
+using System.IO;
+using System.Web.Hosting;
 
 namespace joelrevans.com_v5.Controllers
 {
---
+++ joelrevans.com_v5/joelrevans.com_v5/WebEmbedHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using System.IO;
+
+namespace joelrevans.com_v5
+{
+    public static class WebEmbedHelper
+    {
+        public static MvcHtmlString EmbedTextFile(string path)
+        {
+            string apppath = HostingEnvironment.MapPath("~");
+            string filepath = apppath + path;
+            return new MvcHtmlString(File.ReadAllText(filepath));
+        }
+    }
+}
